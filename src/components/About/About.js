e90a5839539af1de10b14a724c965f3dcccd6064
Juan Carlos Correa <juan.correa.herrera@gmail.com>
2017-06-17 16:14:52 -0500

    Add message in about

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -8,7 +8,13 @@ class About extends React.PureComponent {
   
   render() {
     return (
-      <p>Página de About </p>
+      <main>
+        <h2>About</h2>
+        <h3>Mensaje:</h3>
+        <p>Las novedades de este código es la actualización de PropType como un módulo o paquete de npm independiente de React. </p>
+        <p>Se estructuran las rutas y los contenedores de manera no anidada. Es decir, las rutas están aísladas unas de otras. Se hace el container invocando a los componentes necesarios en cada "vista"</p>
+        <p>Por último, debio a las últimas actualizaciones se cambiaron el modo en que se acceden a algunos props de las rutas y de otros elementos.</p>
+      </main>
     )
   }
 }
